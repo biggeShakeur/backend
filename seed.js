6941cca0497232a04ea37659812cedd5f6935eb4
Jason Wilson <jasonstimphil@gmail.com>
2022-05-02 12:35:46 -0400

    Added models, trip.js, notes.js, dependencies, seed.js, server.js

---
+++ seed.js
@@ -0,0 +1,32 @@
+'use strict'
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+// create a connection to the database long enough to pull the info. Then you can disconnect.
+mongoose.connect(process.env.DB_URL);
+const Maps = require('./models/trip');
+
+async function seed() {
+  // structure is the same as your Book Schema
+  await Maps.create({
+    name: 'Oakland City Hall',
+    osm: 'relation/4682064',
+    xid: 'R4682064',
+    kind: 'architecture,other_buildings_and_structures,historic_architecture,interesting_places',
+    wikidata: 'Q932794',
+    dist: 'asldjfasl',
+    point: {
+      description: 'This was a horrid place',
+      lon: -122.272705,
+      lat: 37.80513
+    }
+  });
+
+  console.log('Your map is functioning properly! Congratulations!!!!!!! LEVEL UP!');
+
+  mongoose.disconnect();
+
+}
+
+seed();
